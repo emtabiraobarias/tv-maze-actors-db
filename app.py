7e4e437075a496b480f3721157004e5e225cd7ae
emtabiraobarias <emtabirao@gmail.com>
2024-05-21 13:58:11 +1000

    Fixed issue with creating db instance on flask app init

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, Blueprint, jsonify
 from flask_restx import Api
-
-db = SQLAlchemy()
-app = Flask(__name__)
-bluePrint = Blueprint('db', __name__, url_prefix='/db')
-api = Api(app)
-app.register_blueprint(bluePrint)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tv-maze-actors.db'
-
-#@app.before_first_request
-#def create_tables():
-#    db.create_all()
+from db import db
+from controller import ns_actor
 
 if __name__ == '__main__':
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tv-maze-actors.db'
     db.init_app(app)
-    app.run(debug=True)
+    with app.app_context():
+        api = Api(app)
+        api.add_namespace(ns_actor, path='/actors')
+        db.create_all()
+        app.run(debug=True)
